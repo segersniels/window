3a17b8d4a8c375434d94a788c16f2e0d6660d904
segersniels <segers.n@hotmail.com>
2020-08-27 15:47:09 +0200

    :fire: Dont include generated files

--- lib/Terminal/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    children: any;
-    style?: any;
-    light?: boolean;
-}
-declare const Terminal: (props: Props) => JSX.Element;
-export default Terminal;
